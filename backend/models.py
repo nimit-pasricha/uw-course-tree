ca98fe45c5d1a44eed883a5e05b9fd0ff6e40f1a
Nimit Pasricha <pasricha.nimit@gmail.com>
2025-11-13 18:11:51 -0600

    Create prereq table.

---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+from typing import List, Optional
+from sqlalchemy import String, Integer, Text, ForeignKey, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+db = SQLAlchemy()
+
+prerequisites = Table(
+    "prerequisites",
+    db.metadata,
+    Column("course_id", Integer, ForeignKey("course.id"), primary_key=True),
+    Column("prereq_id", Integer, ForeignKey("course.id"), primary_key=True)
+)
+
+class Course(db.Model):
+    __tablename__ = "course"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    dept: Mapped[str] = mapped_column(String(10))    # eg: "COMP SCI"
+    number: Mapped[str] = mapped_column(String(10))  # eg: "544"
+    title: Mapped[str] = mapped_column(String(200))  # eg: "Intro to Big Data Systems"
+    description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    credits: Mapped[Optional[str]] = mapped_column(String(20), nullable=True)
+
+    prereqs: Mapped[List["Course"]] = relationship(
+        "Course",
+        secondary=prerequisites,
+        primaryjoin=(prerequisites.c.course_id == id),
+        secondaryjoin=(prerequisites.c.prereq_id == id),
+    )
