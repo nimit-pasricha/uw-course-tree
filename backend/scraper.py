ccfe67b55cfe14ac31ed12e0f0d7b38bf8f121b7
Nimit Pasricha <pasricha.nimit@gmail.com>
2025-11-13 18:57:53 -0600

    Scrape course title.

--- backend/scraper.py
+++ backend/scraper.py
@@ -12,3 +12,20 @@ def scrape_courses():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
     resp = requests.get(URL, headers=headers)
     resp.raise_for_status()
+
+    soup = BeautifulSoup(resp.text, "html.parser")
+
+    course_blocks = soup.find_all("div", class_="courseblock")
+
+    print(f"Found {len(course_blocks)} courses. Parsing...")
+
+    courses_added = 0
+
+    with app.app_context():
+        db.session.query(Course).delete()
+        db.session.commit()
+
+        for block in course_blocks:
+            title_div = block.find("p", class_="courseblocktitle")
+            if not title_div:
+                continue
