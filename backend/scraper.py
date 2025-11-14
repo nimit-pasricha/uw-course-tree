58b73fdd64c3b4b52d6a978e73c6c0ffd936fe7e
Nimit Pasricha <pasricha.nimit@gmail.com>
2025-11-13 19:13:04 -0600

    Delete old data before adding new.

--- backend/scraper.py
+++ backend/scraper.py
@@ -6,6 +6,12 @@ from models import db, Course
 URL = "https://guide.wisc.edu/courses/comp_sci/"
 
 def scrape_courses() -> None:
+
+    print("Deleting old data...")
+    db.session.query(Course).delete()
+    db.session.commit()
+    
+
     print(f"Fetching data from {URL}...")
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
